f6c47c6bd927bbdce63e5ac33d94b20dea1e825b
LuckForgery <dariussiriuse@gmail.com>
2024-11-08 23:20:37 +0000

    Logo icon

--- src/App.js
+++ src/App.js
@@ -102,25 +102,6 @@ function App() {
         <a>Pro. Beyond</a>
       </div>
       <div className=""></div>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <div className="stars">
-            <StarryCanvas className="canvas" />
-          </div>
-        </header>
-      </div>
     </>
   );
 }
