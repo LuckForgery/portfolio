91803de50fa4f528460abf6d878a52da1030736d
LuckForgery <dariussiriuse@gmail.com>
2024-11-08 22:46:34 +0000

    Logo icon

--- src/App.js
+++ src/App.js
@@ -5,25 +5,59 @@ import StarryCanvas from "./StarryCanvas.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="stars">
-          <StarryCanvas className="canvas" />
-        </div>
-      </header>
-    </div>
+    <>
+      <div className="navBar">
+        <table>
+          <tr className="menue">
+            <td id="m1" href="#">
+              <a>Home Section</a>
+            </td>
+            <td id="m2" href="#">
+              <a>Education/Experience</a>
+            </td>
+            <td id="m3" href="#">
+              <a>Active Skills</a>
+            </td>
+            <td id="m4" href="#">
+              <a>My Solutions</a>
+            </td>
+          </tr>
+          <tr className="details">
+            <td id="name">Darius IC Boteand</td>
+            <td id="LinkedIn">
+              <div
+                onClick={() => {
+                  window.open("https://www.linkedin.com/in/darius-ic-boteand");
+                }}
+              >
+                {" "}
+                in
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <div className="stars">
+            <StarryCanvas className="canvas" />
+          </div>
+        </header>
+      </div>
+    </>
   );
 }
 
