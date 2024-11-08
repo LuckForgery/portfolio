04f52f65c5e771bb8ab541aa68147e476845c752
LuckForgery <dariussiriuse@gmail.com>
2024-11-08 22:56:54 +0000

    Logo icon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,41 @@ import "./App.css";
 import StarryCanvas from "./StarryCanvas.jsx";
 
 function App() {
+  const [imageSize, setImageSize] = useState(430);
+  const [imagePosition, setImagePosition] = useState(0); // Add a new state to track the image position
+
+  const divRef = useRef(null);
+  const profileImgRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const rect = divRef.current.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+      const divHeight = rect.height;
+
+      const visibleHeight =
+        Math.min(rect.bottom, windowHeight) - Math.max(rect.top, 0);
+      const visibilityPercentage = visibleHeight / divHeight;
+
+      if (rect.bottom > 3) {
+        setImageSize(70 + visibilityPercentage * 360);
+      } else {
+        setImageSize(70);
+      }
+
+      // Adjust image position
+      const maxTranslation = 150; //maximum translation distance
+      let translateDistance = maxTranslation * (1 - visibilityPercentage);
+      translateDistance = Math.min(translateDistance, maxTranslation);
+
+      setImagePosition(-translateDistance); // Set the translation (negative because it's upward)
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
       <div className="navBar">
@@ -37,7 +72,16 @@ function App() {
           </tr>
         </table>
       </div>
-
+      <img
+        id="profile"
+        ref={profileImgRef}
+        style={{
+          height: `${imageSize}px`,
+          transform: `translateY(${imagePosition}px)`,
+        }}
+        src={process.env.PUBLIC_URL + "/profile.jpeg"}
+        alt="Profile Image"
+      />
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
