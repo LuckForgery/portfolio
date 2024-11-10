923b65253bf630f1be6aebf02f198652766a08d0
LuckForgery <dariussiriuse@gmail.com>
2024-11-10 00:06:56 +0000

    CV file updates

--- src/App.js
+++ src/App.js
@@ -37,6 +37,31 @@ function App() {
 
     window.addEventListener("scroll", handleScroll);
 
+    // Get the 'view-mode' cookie
+    const getCookie = (name) => {
+      let match = document.cookie.match(
+        new RegExp("(^| )" + name + "=([^;]+)"),
+      );
+      return match ? match[2] : null;
+    };
+
+    // Get the 'view-mode' cookie value
+    var viewMode = getCookie("view-mode");
+
+    // Set the viewport content dynamically
+    const viewport = document.querySelector("meta[name='viewport']");
+
+    if (viewport) {
+      if (viewMode === "desktop") {
+        viewport.setAttribute("content", "width=1024");
+      } else if (viewMode === "mobile") {
+        viewport.setAttribute(
+          "content",
+          "width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no",
+        );
+      }
+    }
+
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
