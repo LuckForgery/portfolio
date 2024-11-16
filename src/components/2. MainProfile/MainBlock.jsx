6e21875d5f17a9f1cc4433bb0e9982a10b7f9bca
LuckForgery <dariussiriuse@gmail.com>
2024-11-16 18:00:14 +0000

    Fixes

--- src/components/2. MainProfile/MainBlock.jsx
+++ src/components/2. MainProfile/MainBlock.jsx
@@ -1,12 +1,24 @@
+import React, { useState, useEffect } from "react";
 import StarryCanvas from "./StarryCanvas.jsx";
 
 export default function MainBlock() {
+  const [showCanvas, setShowCanvas] = useState(true);
+
+  useEffect(() => {
+    // Check if the device is a smartphone using the user agent string
+    const isSmartphone = /Mobi|Android/i.test(navigator.userAgent);
+    const isSmallScreen = window.innerWidth <= 600;
+
+    if (isSmartphone && isSmallScreen) {
+      setShowCanvas(false); // Hide canvas on smartphones
+    }
+  }, []);
   return (
     <div className="profile">
       <div className="stars">
-        <StarryCanvas className="canvas" />
+        {showCanvas && <StarryCanvas className="canvas" />}
       </div>
-      <a>Pro. Beyond</a>
+      <section>Pro. Beyond</section>
     </div>
   );
 }
--- src/components/2. MainProfile/ProfileHandler.jsx
+++ src/components/2. MainProfile/ProfileHandler.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const ProfileHandler = (divRef) => {
   const [imageSize, setImageSize] = useState(430);
