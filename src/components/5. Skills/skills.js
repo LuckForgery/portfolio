dc2650182a08470d2c3fc55b899bd5b2944a0400
LuckForgery <dariussiriuse@gmail.com>
2024-12-29 22:42:05 +0200

    Skill Board Setup

--- src/components/5. Skills/skills.js
+++ src/components/5. Skills/skills.js
@@ -10,178 +10,208 @@ import s7 from "./media/skill_7.png";
 import s8 from "./media/skill_8.png";
 
 export const CORE_SKILLS = [
+  // Current Counter : 9
   {
-    image: s1_1,
-    title: "HTML",
-    color: "#ffffff",
-    project: [0, 1],
+    // 29
+    image: s0,
+    title: "test",
+    color: "#161616",
+    project: [],
   },
   {
-    image: s1_2,
-    title: "CSS",
-    color: "#ffffff",
-    project: [0, 1],
+    // 18
+    image: s0,
+    title: "test",
+    color: "#161616",
+    project: [],
   },
   {
-    image: s2,
-    title: "JS",
-    color: "#ffffff",
-    project: [0, 1],
+    // 19
+    image: s0,
+    title: "test",
+    color: "#161616",
+    project: [],
   },
   {
-    image: s3,
-    title: "React",
-    color: "#ffffff",
-    project: [0, 1],
-  },
-  {
-    image: s4,
-    title: "React Native",
-    color: "#222222",
-    project: [1],
-  },
-  {
-    image: s5,
-    title: "Python",
-    color: "#ffffff",
-    project: [0],
-  },
-  {
-    image: s6,
-    title: "FastAPI",
-    color: "#ffffff",
-    project: [0],
-  },
-  {
+    // 8
     image: s7,
     title: "MariaDB",
     color: "#0472d1",
     project: [0],
   },
   {
+    // 20
+    image: s0,
+    title: "test",
+    color: "#161616",
+    project: [],
+  },
+  {
+    // 28
+    image: s0,
+    title: "test",
+    color: "#161616",
+    project: [],
+  },
+  {
+    // 17
+    image: s0,
+    title: "test",
+    color: "#161616",
+    project: [],
+  },
+  {
+    // 6
+    image: s5,
+    title: "Python",
+    color: "#ffffff",
+    project: [0],
+  },
+  {
+    // 7
+    image: s6,
+    title: "FastAPI",
+    color: "#ffffff",
+    project: [0],
+  },
+  {
+    // 9
     image: s8,
     title: "RaspberryPi",
     color: "#bc1043",
     project: [0],
   },
   {
+    // 21
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 27
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 16
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 5
+    image: s4,
+    title: "React Native",
+    color: "#222222",
+    project: [1],
+  },
+  {
+    // 1
+    image: s1_1,
+    title: "HTML",
+    color: "#ffffff",
+    project: [0, 1],
+  },
+  {
+    // 2
+    image: s1_2,
+    title: "CSS",
+    color: "#ffffff",
+    project: [0, 1],
+  },
+  {
+    // 10
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 22
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 26
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 15
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 4
+    image: s3,
+    title: "React",
+    color: "#ffffff",
+    project: [0, 1],
+  },
+  {
+    // 3
+    image: s2,
+    title: "JS",
+    color: "#ffffff",
+    project: [0, 1],
+  },
+  {
+    // 11
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 23
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 25
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 14
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 13
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 12
     image: s0,
     title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
   {
+    // 24
     image: s0,
     title: "test",
-    color: "#282c33",
-    project: [],
-  },
-  {
-    image: s0,
-    title: "test",
-    color: "#282c33",
-    project: [],
-  },
-  {
-    image: s0,
-    title: "test",
-    color: "#282c33",
-    project: [],
-  },
-  {
-    image: s0,
-    title: "test",
-    color: "#282c33",
-    project: [],
-  },
-  {
-    image: s0,
-    title: "test",
-    color: "#282c33",
-    project: [],
-  },
-  {
-    image: s0,
-    title: "test",
-    color: "#282c33",
-    project: [],
-  },
-  {
-    image: s0,
-    title: "test",
-    color: "#282c33",
+    color: "#161616",
     project: [],
   },
 ];
