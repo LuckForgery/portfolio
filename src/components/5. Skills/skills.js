4fd506fb0b73c666ab9b4d58f58fc266903686ed
LuckForgery <dariussiriuse@gmail.com>
2024-12-29 23:45:16 +0200

    Skill Board Setup

--- src/components/5. Skills/skills.js
+++ src/components/5. Skills/skills.js
@@ -1,43 +1,49 @@
 import s0 from "./media/skill_0_DUMMY.png";
-import s1_1 from "./media/skill_1_1.png";
-import s1_2 from "./media/skill_1_2.png";
-import s2 from "./media/skill_2.png";
-import s3 from "./media/skill_3.png";
-import s4 from "./media/skill_4.png";
-import s5 from "./media/skill_5.png";
-import s6 from "./media/skill_6.png";
-import s7 from "./media/skill_7.png";
-import s8 from "./media/skill_8.png";
+import s1 from "./media/skill_1_1.png";
+import s2 from "./media/skill_1_2.png";
+import s3 from "./media/skill_2.png";
+import s4 from "./media/skill_3.png";
+import s5 from "./media/skill_4.png";
+import s6 from "./media/skill_5.png";
+import s7 from "./media/skill_6.png";
+import s8 from "./media/skill_10.png"; // snowflake
+import s9 from "./media/skill_7.png";
+import s10 from "./media/skill_8.png";
+import s13 from "./media/skill_13.png"; // servicenow
+import s17 from "./media/skill_17.png"; // selenium
+import s18 from "./media/skill_18.png"; // java
+import s19 from "./media/skill_19.png"; // springboot
+import s29 from "./media/skill_29.png"; // claimcenter
 
 export const CORE_SKILLS = [
   // Current Counter : 9
   {
     // 29
-    image: s0,
-    title: "test",
-    color: "#161616",
+    image: s29,
+    title: "ClaimCenter",
+    color: "#454545",
     project: [],
   },
   {
     // 18
-    image: s0,
-    title: "test",
-    color: "#161616",
+    image: s18,
+    title: "Java",
+    color: "#ffffff",
     project: [],
   },
   {
     // 19
-    image: s0,
-    title: "test",
-    color: "#161616",
+    image: s19,
+    title: "SpringBoot",
+    color: "#8bc44a",
     project: [],
   },
   {
     // 8
-    image: s7,
-    title: "MariaDB",
-    color: "#0472d1",
-    project: [0],
+    image: s8,
+    title: "Snowflake",
+    color: "#282828",
+    project: [],
   },
   {
     // 20
@@ -55,30 +61,30 @@ export const CORE_SKILLS = [
   },
   {
     // 17
-    image: s0,
-    title: "test",
+    image: s17,
+    title: "Selenium",
     color: "#161616",
     project: [],
   },
   {
     // 6
-    image: s5,
+    image: s6,
     title: "Python",
     color: "#ffffff",
     project: [0],
   },
   {
     // 7
-    image: s6,
+    image: s7,
     title: "FastAPI",
     color: "#ffffff",
     project: [0],
   },
   {
     // 9
-    image: s8,
+    image: s9,
     title: "RaspberryPi",
-    color: "#bc1043",
+    color: "#0472d1",
     project: [0],
   },
   {
@@ -104,31 +110,31 @@ export const CORE_SKILLS = [
   },
   {
     // 5
-    image: s4,
+    image: s5,
     title: "React Native",
     color: "#222222",
     project: [1],
   },
   {
     // 1
-    image: s1_1,
+    image: s1,
     title: "HTML",
     color: "#ffffff",
     project: [0, 1],
   },
   {
     // 2
-    image: s1_2,
+    image: s2,
     title: "CSS",
     color: "#ffffff",
     project: [0, 1],
   },
   {
     // 10
-    image: s0,
-    title: "test",
-    color: "#161616",
-    project: [],
+    image: s10,
+    title: "MariaDB",
+    color: "#bc1043",
+    project: [0],
   },
   {
     // 22
@@ -153,14 +159,14 @@ export const CORE_SKILLS = [
   },
   {
     // 4
-    image: s3,
+    image: s4,
     title: "React",
     color: "#ffffff",
     project: [0, 1],
   },
   {
     // 3
-    image: s2,
+    image: s3,
     title: "JS",
     color: "#ffffff",
     project: [0, 1],
@@ -195,9 +201,9 @@ export const CORE_SKILLS = [
   },
   {
     // 13
-    image: s0,
-    title: "test",
-    color: "#161616",
+    image: s13,
+    title: "ServiceNow",
+    color: "#00393d",
     project: [],
   },
   {
